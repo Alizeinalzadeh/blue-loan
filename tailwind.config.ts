6286377b959e01265dfc2748b9bd16ad4fc4ac1b
alizeinalzadeh <ali94zeinalzadeh@gmail.com>
2024-07-15 14:26:46 +0330

    config(global): add blue color system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,74 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	important: true,
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		colors: {
+			primary: {
+				main: '#75AAEB',
+				light: '#2B3440',
+			},
+			background: {
+				main: '#1B2028',
+			},
+			surface: {
+				main: '#1B2028',
+				light: '#2B3340',
+				disable: '#5C6D88',
+				brand: {
+					light: '#2B3440',
+					DEFAULT: '#75AAEB',
+				},
+				background: '#1B2028',
+				primary: '#1B2028',
+				secondary: '#2B3340',
+				negative: {
+					light: '#402B2F',
+					DEFAULT: '#EB758B',
+				},
+				positive: {
+					light: '#2B403B',
+					DEFAULT: '#00BD92',
+				},
+				warning: {
+					light: '#2B403B',
+					DEFAULT: '#EBD675',
+				},
+				highlight: {
+					positive: '#154641',
+					negative: '#6B2F3B',
+					warning: '#4D441C',
+				},
+			},
+			negative: {
+				main: '#EB758B',
+				light: '#402B2F',
+				highlight: '#6B2F3B',
+			},
+			positive: {
+				main: '#00BD92',
+				light: '#2B403B',
+				highlight: '#154641',
+			},
+			warning: {
+				main: '#EBD675',
+				light: '#403C2B',
+				highlight: '#4D441C',
+			},
+			content: {
+				primary: '#E7EAEF',
+				secondary: '#9FABBE',
+				positive: '#00bd92',
+				warning: '#ebd675',
+			},
+			overlay: 'rgba(12, 16, 17, 0.7)',
+		},
+	},
+	plugins: [],
 };
 export default config;
