da891c449820194e7a6f597301a5f7a5d2f3aacb
alizeinalzadeh <ali94zeinalzadeh@gmail.com>
2024-07-17 17:25:58 +0330

    feat(global): loans list page

---
+++ src/app/list/page.tsx
@@ -0,0 +1,10 @@
+import Container from '@/components/Layout/Global/Container/Container';
+import LoanList from '@/components/Pages/Loan/List/LoansList/LoanList';
+
+export default function ListPage() {
+	return (
+		<Container>
+			<LoanList />
+		</Container>
+	);
+}
